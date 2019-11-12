0034d02720e4dac8bafa158719390d8cc2e8e897
Vít Vašák <vitvasak@outlook.com>
2019-11-12 19:13:02 +0100

    DarkMode

--- FutbalnetApp/FutbalnetApp/FutbalnetApp/ViewModels/CompetitionDetailViewModel.cs
+++ FutbalnetApp/FutbalnetApp/FutbalnetApp/ViewModels/CompetitionDetailViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
---
+++ FutbalnetApp/FutbalnetApp/FutbalnetApp/Views/SettingsPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FutbalnetApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SettingsPage : ContentPage
+	{
+		public SettingsPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Switch_Toggled(object sender, ToggledEventArgs e)
+		{
+			if (e.Value == true)
+			{
+				Application.Current.Resources["BackgroundColor"] = Application.Current.Resources["BackgroundColorDark"];
+				Application.Current.Resources["AlternativeColor"] = Application.Current.Resources["AlternativeColorDark"];
+				//Application.Current.Resources["DarkAlternativeColor"] = Application.Current.Resources["DarkAlternativeColorDark"];
+				Application.Current.Resources["TextColor"] = Application.Current.Resources["TextColorDark"];
+			}
+			else
+			{
+				Application.Current.Resources["BackgroundColor"] = Application.Current.Resources["BackgroundColorLight"];
+				Application.Current.Resources["AlternativeColor"] = Application.Current.Resources["AlternativeColorLight"];
+				//Application.Current.Resources["DarkAlternativeColor"] = Application.Current.Resources["DarkAlternativeColorLight"];
+				Application.Current.Resources["TextColor"] = Application.Current.Resources["TextColorLight"];
+			}
+		}
+	}
+}
