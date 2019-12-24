6ee04d0782c947af929e47b41e7e938300f22149
Vít Vašák <vitvasak@outlook.com>
2019-12-24 14:17:03 +0100

    SearchPage

---
+++ FutbalnetApp/FutbalnetApp/FutbalnetApp/ViewModels/SearchViewModel.cs
@@ -0,0 +1,81 @@
+﻿using FutbalnetApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FutbalnetApp.ViewModels
+{
+	public class SearchViewModel : BaseViewModel
+	{
+		public ObservableCollection<SearchResult> Clubs { get; set; }
+		public ObservableCollection<SearchResult> Persons { get; set; }
+		public ObservableCollection<SearchResult> Competitions { get; set; }
+		public ObservableCollection<SearchResult> Unions { get; set; }
+		public Command LoadSearchResultsCommand { get; set; }
+		public int SelectedIndex { get; set; }
+
+
+
+		public SearchViewModel()
+		{
+			LoadSearchResultsCommand = new Command<string>(async (term) => await ExecuteLoadSearchResultsCommand(term));
+			Clubs = new ObservableCollection<SearchResult>();
+			Persons = new ObservableCollection<SearchResult>();
+			Competitions = new ObservableCollection<SearchResult>();
+			Unions = new ObservableCollection<SearchResult>();
+		}
+
+		async Task ExecuteLoadSearchResultsCommand(string term)
+		{
+			if (IsBusy)
+				return;
+
+			IsBusy = true;
+
+			try
+			{
+				var results = await SportnetStore.GetSearchResults(term);
+				
+				Clubs.Clear();
+				Persons.Clear();
+				Competitions.Clear();
+				Unions.Clear();
+				
+				foreach (var item in results)
+				{
+					switch (item.Type)
+					{
+						case "person":
+							Persons.Add(item);
+							break;
+						case "club":
+							Clubs.Add(item);
+							break;
+						case "competition":
+							Competitions.Add(item);
+							break;
+						case "union":
+							Unions.Add(item);
+							break;
+						default:
+							break;
+					}
+				}
+
+				IsLoaded = true;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+		}
+	}
+}
---
+++ FutbalnetApp/FutbalnetApp/FutbalnetApp/Views/SearchPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using FutbalnetApp.Models;
+using FutbalnetApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FutbalnetApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SearchPage : ContentPage
+	{
+		SearchViewModel viewModel;
+
+		public SearchPage()
+		{
+			InitializeComponent();
+			BindingContext = viewModel = new SearchViewModel();
+		}
+
+		private async void PersonsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (!(e.CurrentSelection.FirstOrDefault() is SearchResult item))
+				return;
+
+			await Navigation.PushAsync(new PersonDetailPage(item.Id));
+			PersonsList.SelectedItem = null;
+		}
+
+		private async void ClubsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (!(e.CurrentSelection.FirstOrDefault() is SearchResult item))
+				return;
+
+			await Navigation.PushAsync(new ClubDetailPage(item.Id));
+			ClubsList.SelectedItem = null;
+		}
+
+		private async void CompetitionsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (!(e.CurrentSelection.FirstOrDefault() is SearchResult item))
+				return;
+
+			await Navigation.PushAsync(new CompetitionDetailPage(item.Id));
+			CompetitionsList.SelectedItem = null;
+		}
+
+		private async void UnionsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (!(e.CurrentSelection.FirstOrDefault() is SearchResult item))
+				return;
+
+			//await Navigation.PushAsync(new UnionDetailPage(item.Id));
+			UnionsList.SelectedItem = null;
+		}
+
+		private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (SearchBar.Text?.Length > 2)
+			{
+				viewModel.LoadSearchResultsCommand.Execute(SearchBar.Text);
+			}
+		}
+	}
+}
