d03795ceef7906f3da2f66aeb9e81419dc14dc85
Vít Vašák <vitvasak@outlook.com>
2019-12-25 23:02:41 +0100

    sending mail

--- FutbalnetApp/FutbalnetApp/FutbalnetApp/Views/SettingsPage.xaml.cs
+++ FutbalnetApp/FutbalnetApp/FutbalnetApp/Views/SettingsPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,5 +17,59 @@ namespace FutbalnetApp.Views
 		{
 			InitializeComponent();
 		}
+
+        private void FeedbackSendClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var mail = new MailMessage();
+                var SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("vitvasakport@gmail.com");
+                mail.To.Add("vitvasak@outlook.com");
+                mail.Subject = "FutbalVille-Feedback";
+                mail.Body = FeedbackEditor.Text;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Host = "smtp.gmail.com";
+                SmtpServer.EnableSsl = true;
+                SmtpServer.UseDefaultCredentials = false;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("vitvasakport@gmail.com", "vvport1598");
+
+                SmtpServer.Send(mail);
+                DisplayAlert("Odoslané", "Ďakujeme za váš názor", "OK");
+
+            }
+            catch (Exception)
+            {
+                DisplayAlert("Chyba", "Odosielanie zlyhalo", "OK");
+            }
+        }
+        private void BugSendClicked(object sender, EventArgs e)
+		{
+            try
+            {
+                var mail = new MailMessage();
+                var SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("vitvasakport@gmail.com");
+                mail.To.Add("vitvasak@outlook.com");
+                mail.Subject = "FutbalVille-Bug";
+                mail.Body = BugEditor.Text;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Host = "smtp.gmail.com";
+                SmtpServer.EnableSsl = true;
+                SmtpServer.UseDefaultCredentials = false;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("vitvasakport@gmail.com", "vvport1598");
+
+                SmtpServer.Send(mail);
+                DisplayAlert("Odoslané", "Ďakujeme za upozornenie na problém", "OK");
+            }
+            catch (Exception)
+            {
+                DisplayAlert("Chyba", "Odosielanie zlyhalo", "OK");
+            }
+        }
 	}
 }
