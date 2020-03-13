b8e515415607bae2ecabd406e791ee4b88afd29c
Vít Vašák <vitvasak@outlook.com>
2020-03-13 14:05:57 +0100

    Android portrait lock, version increment

--- FutbalnetApp/FutbalnetApp/FutbalnetApp.Android/MainActivity.cs
+++ FutbalnetApp/FutbalnetApp/FutbalnetApp.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Android.Gms.Ads;
 
 namespace FutbalnetApp.Droid
 {
-    [Activity(Label = "FutbalVille", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "FutbalVille", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
